20f87ddd1d2167ae9557febd45ec8d3e1a7a778f
sébastien Legait <sebastien.legait@isen.yncrea.fr>
2020-11-02 10:54:55 +0100

    contributor is able to see comments ,comments and demete its own comment , corrected some bug in challenge.py

--- CTFd/api/v1/challenges.py
+++ CTFd/api/v1/challenges.py
@@ -424,7 +424,7 @@ class Challenge(Resource):
     )
     def patch(self, challenge_id):
         author_id = session["id"]
-        if is_admin() or is_contributor_plus or (is_contributor() and self.author_id==author_id):
+        if is_admin() or is_contributor_plus() or (is_contributor() and self.author_id==author_id):
             challenge = Challenges.query.filter_by(id=challenge_id).first_or_404()
             challenge_class = get_chal_class(challenge.type)
             challenge = challenge_class.update(challenge, request)
