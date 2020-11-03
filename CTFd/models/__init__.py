3c13595bf38816bbd5fa6c1614c5bb2888f02bf7
sébastien Legait <sebastien.legait@isen.yncrea.fr>
2020-11-03 15:27:23 +0100

    debug users display in admin panel

--- CTFd/models/__init__.py
+++ CTFd/models/__init__.py
@@ -319,9 +319,7 @@ class Users(db.Model):
     def awards(self):
         return self.get_awards(admin=False)
 
-    @property
-    def score(self):
-        return self.get_score(admin=False)
+  
 
     @property
     def place(self):
@@ -370,38 +368,7 @@ class Users(db.Model):
             awards = awards.filter(Awards.date < dt)
         return awards.all()
 
-    @cache.memoize()
-    def get_score(self, admin=False):
-        score = db.func.sum(Challenges.value).label("score")
-        user = (
-            db.session.query(Solves.user_id, score)
-            .join(Users, Solves.user_id == Users.id)
-            .join(Challenges, Solves.challenge_id == Challenges.id)
-            .filter(Users.id == self.id)
-        )
-
-        award_score = db.func.sum(Awards.value).label("award_score")
-        award = db.session.query(award_score).filter_by(user_id=self.id)
-
-        if not admin:
-            freeze = Configs.query.filter_by(key="freeze").first()
-            if freeze and freeze.value:
-                freeze = int(freeze.value)
-                freeze = datetime.datetime.utcfromtimestamp(freeze)
-                user = user.filter(Solves.date < freeze)
-                award = award.filter(Awards.date < freeze)
-
-        user = user.group_by(Solves.user_id).first()
-        award = award.first()
-
-        if user and award:
-            return int(user.score or 0) + int(award.award_score or 0)
-        elif user:
-            return int(user.score or 0)
-        elif award:
-            return int(award.award_score or 0)
-        else:
-            return 0
+  
 
     @cache.memoize()
     def get_place(self, admin=False, numeric=False):
