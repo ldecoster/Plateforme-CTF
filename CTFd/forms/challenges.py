42b536eb854803a670da1982717d03adc9cd2c47
linakda <40620181+linakda@users.noreply.github.com>
2021-01-11 15:45:50 +0100

    FilterForm for challenges

--- CTFd/forms/challenges.py
+++ CTFd/forms/challenges.py
@@ -4,6 +4,21 @@ from wtforms.validators import InputRequired
 from CTFd.forms import BaseForm
 from CTFd.forms.fields import SubmitField
 
+class ChallengesFilterForm(BaseForm):
+    field = SelectField(
+        "Search Field",
+        choices=[
+            ("name", "Name"),
+            ("author", "Author"),
+            ("tags", "Tag"),
+            ("type", "Type"),
+            ("sucess", "Challenge Succeded"),
+        ],
+        default="name",
+        validators=[InputRequired()],
+    )
+    q = StringField("Parameter", validators=[InputRequired()])
+    submit = SubmitField("Search")
 
 class ChallengeSearchForm(BaseForm):
     field = SelectField(
