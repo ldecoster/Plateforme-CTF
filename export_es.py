4b934cfd0ef236a9dfc140a68d25991cc496e5d2
ldecoster <loic.decoster@isen.yncrea.fr>
2021-05-21 15:36:31 +0200

    Add dependencies to requirements.txt and requirements.in. Remove unnecessary connections and cursors in export function. Code refactoring

--- export_es.py
+++ export_es.py
@@ -1,202 +1,199 @@
-import sqlite3
-import csv
-import schedule
-import time
+from CTFd import create_app
 from elasticsearch import helpers, Elasticsearch
+from CTFd.utils import get_app_config
+
+import csv
 import pandas as pd
+import schedule
+import sqlite3
+import time
 
 
 def export():
-    db = "CTFd/ctfd.db"
-    # Users
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur.execute("SELECT * from Users")
-    result = cur.fetchall()
-    cur.execute("SELECT name FROM pragma_table_info('users')")
+    app = create_app()
+    with app.app_context():
+        db = get_app_config("SQLALCHEMY_DATABASE_URI").replace("sqlite:///", "")
+        conn = sqlite3.connect(db)
+        cur = conn.cursor()
 
-    header_list = cur.fetchall()
-    with open('user.csv', 'w', newline='') as csv_file:
-        c = csv.writer(csv_file, delimiter=',')
-        c.writerow(header_list)
-        for x in result:
-            c.writerow(x)
+        # Users
+        cur.execute("SELECT * from users")
+        result = cur.fetchall()
+        cur.execute("SELECT name FROM pragma_table_info('users')")
+        header_list = cur.fetchall()
 
-    # Votes
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur.execute("SELECT * from Votes")
-    result = cur.fetchall()
-    cur.execute("SELECT name FROM pragma_table_info('votes')")
+        with open('user.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            c = csv.writer(csv_file, delimiter=',')
+            c.writerow(header_list)
+            for x in result:
+                c.writerow(x)
 
-    header_list = cur.fetchall()
-    with open('vote.csv', 'w', newline='') as csv_file:
-        c = csv.writer(csv_file, delimiter=',')
-        c.writerow(header_list)
-        for x in result:
-            c.writerow(x)
+        # Votes
+        cur.execute("SELECT * from votes")
+        result = cur.fetchall()
+        cur.execute("SELECT name FROM pragma_table_info('votes')")
+        header_list = cur.fetchall()
 
-    # Challenges
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur.execute("SELECT * from Challenges")
-    result = cur.fetchall()
-    cur.execute("SELECT name FROM pragma_table_info('challenges')")
+        with open('vote.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            c = csv.writer(csv_file, delimiter=',')
+            c.writerow(header_list)
+            for x in result:
+                c.writerow(x)
 
-    header_list = cur.fetchall()
-    with open('challenge.csv', 'w', newline='') as csv_file:
-        c = csv.writer(csv_file, delimiter=',')
-        c.writerow(header_list)
-        for x in result:
-            c.writerow(x)
+        # Challenges
+        cur.execute("SELECT * from challenges")
+        result = cur.fetchall()
+        cur.execute("SELECT name FROM pragma_table_info('challenges')")
+        header_list = cur.fetchall()
 
-    # Solves
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur.execute("SELECT * from Solves")
-    result = cur.fetchall()
-    cur.execute("SELECT name FROM pragma_table_info('solves')")
+        with open('challenge.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            c = csv.writer(csv_file, delimiter=',')
+            c.writerow(header_list)
+            for x in result:
+                c.writerow(x)
 
-    header_list = cur.fetchall()
-    with open('solve.csv', 'w', newline='') as csv_file:
-        c = csv.writer(csv_file, delimiter=',')
-        c.writerow(header_list)
-        for x in result:
-            c.writerow(x)
+        # Solves
+        cur.execute("SELECT * from solves")
+        result = cur.fetchall()
+        cur.execute("SELECT name FROM pragma_table_info('solves')")
+        header_list = cur.fetchall()
 
-    # Submission
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur.execute("SELECT * from Submissions")
-    result = cur.fetchall()
-    cur.execute("SELECT name FROM pragma_table_info('submissions')")
+        with open('solve.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            c = csv.writer(csv_file, delimiter=',')
+            c.writerow(header_list)
+            for x in result:
+                c.writerow(x)
 
-    header_list = cur.fetchall()
-    with open('submission.csv', 'w', newline='') as csv_file:
-        c = csv.writer(csv_file, delimiter=',')
-        c.writerow(header_list)
-        for x in result:
-            c.writerow(x)
+        # Submission
+        cur.execute("SELECT * from submissions")
+        result = cur.fetchall()
+        cur.execute("SELECT name FROM pragma_table_info('submissions')")
+        header_list = cur.fetchall()
 
-    # Tags
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur.execute("SELECT * from Tags")
-    result = cur.fetchall()
-    cur.execute("SELECT name FROM pragma_table_info('tags')")
+        with open('submission.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            c = csv.writer(csv_file, delimiter=',')
+            c.writerow(header_list)
+            for x in result:
+                c.writerow(x)
 
-    header_list = cur.fetchall()
-    with open('tag.csv', 'w', newline='') as csv_file:
-        c = csv.writer(csv_file, delimiter=',')
-        c.writerow(header_list)
-        for x in result:
-            c.writerow(x)
+        # Tags
+        cur.execute("SELECT * from tags")
+        result = cur.fetchall()
+        cur.execute("SELECT name FROM pragma_table_info('tags')")
+        header_list = cur.fetchall()
 
-    # TagChallenge
-    conn = sqlite3.connect(db)
-    cur = conn.cursor()
-    cur.execute("SELECT * from TagChallenge")
-    result = cur.fetchall()
-    cur.execute("SELECT name FROM pragma_table_info('tagChallenge')")
+        with open('tag.csv', 'w', newline='', encoding='utf-8') as csv_file:
+            c = csv.writer(csv_file, delimiter=',')
+            c.writerow(header_list)
+            for x in result:
+                c.writerow(x)
 
-    header_list = cur.fetchall()
-    with open('tagChallenge.csv', 'w', newline='') as csvFile:
-        c = csv.writer(csvFile, delimiter=',')
-        c.writerow(header_list)
-        for x in result:
-            c.writerow(x)
+        # TagChallenge
+        cur.execute("SELECT * from tagChallenge")
+        result = cur.fetchall()
+        cur.execute("SELECT name FROM pragma_table_info('tagChallenge')")
+        header_list = cur.fetchall()
 
-    # Merge Submissions and Challenges
-    sub = pd.read_csv("submission.csv", sep=',')
-    chal = pd.read_csv("challenge.csv", sep=',')
+        with open('tagChallenge.csv', 'w', newline='', encoding='utf-8') as csvFile:
+            c = csv.writer(csvFile, delimiter=',')
+            c.writerow(header_list)
+            for x in result:
+                c.writerow(x)
 
-    challenge = chal.rename(columns={"('id',)": "('challenge_id',)"})
+        # Closing connection
+        conn.close()
 
-    challenge = challenge.drop(columns=["('type',)", ], axis=1)
+        # Merge Submissions and Challenges
+        sub = pd.read_csv("submission.csv", sep=',')
+        chal = pd.read_csv("challenge.csv", sep=',')
 
-    submission = sub.set_index("('challenge_id',)")
-    challenges = challenge.set_index("('challenge_id',)")
+        challenge = chal.rename(columns={"('id',)": "('challenge_id',)"})
 
-    df_mixed_chal_sub = submission.join(challenges)
+        challenge = challenge.drop(columns=["('type',)", ], axis=1)
 
-    df_mixed_chal_sub.to_csv("chal&sub.csv", sep=',')
+        submission = sub.set_index("('challenge_id',)")
+        challenges = challenge.set_index("('challenge_id',)")
 
-    # Merge Submissions and Users
-    user = pd.read_csv("user.csv", sep=',', encoding="ISO-8859-1")
-    user = user.rename(columns={"('id',)": "('user_id',)"})
-    user = user.drop(columns=["('type',)", ], axis=1)
+        df_mixed_chal_sub = submission.join(challenges)
 
-    submission = sub.set_index("('user_id',)")
-    users = user.set_index("('user_id',)")
+        df_mixed_chal_sub.to_csv("chal&sub.csv", sep=',')
 
-    df_mixed_user_sub = submission.join(users)
+        # Merge Submissions and Users
+        user = pd.read_csv("user.csv", sep=',', encoding='utf-8')
+        user = user.rename(columns={"('id',)": "('user_id',)"})
+        user = user.drop(columns=["('type',)", ], axis=1)
 
-    df_mixed_user_sub.to_csv("user&sub.csv", sep=',')
+        submission = sub.set_index("('user_id',)")
+        users = user.set_index("('user_id',)")
 
-    # Merge Tag and TagChallenge
-    tag = pd.read_csv("tag.csv", sep=',', encoding='latin-1')
-    tagchal = pd.read_csv("tagChallenge.csv", sep=',', encoding='latin-1')
+        df_mixed_user_sub = submission.join(users)
 
-    tags = tag.rename(columns={"('id',)": "('tag_id',)"})
+        df_mixed_user_sub.to_csv("user&sub.csv", sep=',')
 
-    tagChallenge = tagchal.set_index("('tag_id',)")
-    tagss = tags.set_index("('tag_id',)")
+        # Merge Tag and TagChallenge
+        tag = pd.read_csv("tag.csv", sep=',', encoding='utf-8')
+        tagchal = pd.read_csv("tagChallenge.csv", sep=',', encoding='utf-8')
 
-    df_mixed_tag_chal = tagChallenge.join(tagss)
+        tags = tag.rename(columns={"('id',)": "('tag_id',)"})
 
-    df_mixed_tag_chal.to_csv("merge_tagchal.csv", sep=',')
+        tagChallenge = tagchal.set_index("('tag_id',)")
+        tagss = tags.set_index("('tag_id',)")
 
-    # ES connection
-    es = Elasticsearch()
-    with open('submission.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="submission"):
-            es.indices.delete(index='submission')
-        helpers.bulk(es, reader, index='submission', doc_type='my-type')
+        df_mixed_tag_chal = tagChallenge.join(tagss)
 
-    with open('solve.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="solve"):
-            es.indices.delete(index='solve')
-        helpers.bulk(es, reader, index='solve', doc_type='my-type')
+        df_mixed_tag_chal.to_csv("merge_tagchal.csv", sep=',')
 
-    with open('challenge.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="challenge"):
-            es.indices.delete(index='challenge')
-        helpers.bulk(es, reader, index='challenge', doc_type='my-type')
+        # ES connection
+        es = Elasticsearch()
+        with open('submission.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="submission"):
+                es.indices.delete(index='submission')
+            helpers.bulk(es, reader, index='submission', doc_type='my-type')
 
-    with open('vote.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="vote"):
-            es.indices.delete(index='vote')
-        helpers.bulk(es, reader, index='vote', doc_type='my-type')
+        with open('solve.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="solve"):
+                es.indices.delete(index='solve')
+            helpers.bulk(es, reader, index='solve', doc_type='my-type')
 
-    with open('user.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="user"):
-            es.indices.delete(index='user')
-        helpers.bulk(es, reader, index='user', doc_type='my-type')
+        with open('challenge.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="challenge"):
+                es.indices.delete(index='challenge')
+            helpers.bulk(es, reader, index='challenge', doc_type='my-type')
 
-    with open('chal&sub.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="chal&sub"):
-            es.indices.delete(index='chal&sub')
-        helpers.bulk(es, reader, index='chal&sub', doc_type='my-type')
+        with open('vote.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="vote"):
+                es.indices.delete(index='vote')
+            helpers.bulk(es, reader, index='vote', doc_type='my-type')
 
-    with open('user&sub.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="user&sub"):
-            es.indices.delete(index='user&sub')
-        helpers.bulk(es, reader, index='user&sub', doc_type='my-type')
+        with open('user.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="user"):
+                es.indices.delete(index='user')
+            helpers.bulk(es, reader, index='user', doc_type='my-type')
 
-    with open('merge_tagchal.csv') as f:
-        reader = csv.DictReader(f)
-        if es.indices.exists(index="merge_tagchal"):
-            es.indices.delete(index='merge_tagchal')
-        helpers.bulk(es, reader, index='merge_tagchal', doc_type='my-type')
+        with open('chal&sub.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="chal&sub"):
+                es.indices.delete(index='chal&sub')
+            helpers.bulk(es, reader, index='chal&sub', doc_type='my-type')
 
-    print("Data updated")
+        with open('user&sub.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="user&sub"):
+                es.indices.delete(index='user&sub')
+            helpers.bulk(es, reader, index='user&sub', doc_type='my-type')
+
+        with open('merge_tagchal.csv', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            if es.indices.exists(index="merge_tagchal"):
+                es.indices.delete(index='merge_tagchal')
+            helpers.bulk(es, reader, index='merge_tagchal', doc_type='my-type')
+
+        print("Data updated")
 
 
 export()
